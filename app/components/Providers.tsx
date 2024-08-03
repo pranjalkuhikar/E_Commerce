850a2f25bc4a4d953e678347c9e5dbe7b5483e14
pranjalkuhikar <pranjalkuhikar123@gmail.com>
2024-08-03 15:31:58 +0530

    "Update successUrl and cancelUrl in CartProvider from localhost to Vercel app URLs"

--- app/components/Providers.tsx
+++ app/components/Providers.tsx
@@ -7,8 +7,8 @@ export default function CartProvider({ children }: { children: ReactNode }) {
       mode="payment"
       cartMode="client-only"
       stripe={process.env.NEXT_PUBLIC_STRIPE_KEY as string}
-      successUrl="http://localhost:3000/stripe/success"
-      cancelUrl="http://localhost:3000/stripe/error"
+      successUrl="https://e-commerce-eosin-seven-90.vercel.app/stripe/success"
+      cancelUrl="https://e-commerce-eosin-seven-90.vercel.app/stripe/error"
       currency="INR"
       billingAddressCollection={false}
       shouldPersist={true}
